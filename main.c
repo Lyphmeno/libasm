b20f3112cf9b2461db592ba84591084ba094166d
Lyphmeno <hugolevipro@gmail.com>
2024-01-03 15:46:25 +0100

    better main

--- main.c
+++ main.c
@@ -8,23 +8,27 @@ int main(void)
     printf("----------------------STRLEN-----------------------\n");
     printf("\033[0;33m\"test\"\t= %ld\n", strlen("test"));
     printf("\033[0;32m\"test\"\t= %ld\n", ft_strlen("test"));
+    printf("\033[0;33m\"\"\t= %ld\n", strlen(""));
+    printf("\033[0;32m\"\"\t= %ld\n", ft_strlen(""));
 
     printf("----------------------STRCPY-----------------------\n");
     char dest[] = "test : ";
     char dest0[] = "test : ";
     char src[] = "truc";
-    printf("\033[0;33m%s\n", strcpy(dest, src));
-    printf("\033[0;32m%s\n", ft_strcpy(dest0, src));
+    printf("\033[0;33m-> |%s|\n", strcpy(dest, src));
+    printf("\033[0;32m-> |%s|\n", ft_strcpy(dest0, src));
+    printf("\033[0;33m-> |%s|\n", strcpy(dest, ""));
+    printf("\033[0;32m-> |%s|\n", ft_strcpy(dest0, ""));
 
     printf("----------------------STRCMP-----------------------\n");
     char dest1[] = "123456";
     char dest2[] = "1234567";
-    printf("\033[0;33m\"equal\"\t= %d\n", strcmp(dest1, dest1));
-    printf("\033[0;32m\"equal\"\t= %d\n", ft_strcmp(dest1, dest1));
-    printf("\033[0;33m\"less\"\t= %d\n", strcmp(dest1, dest2));
-    printf("\033[0;32m\"less\"\t= %d\n", ft_strcmp(dest1, dest2));
-    printf("\033[0;33m\"more\"\t= %d\n", strcmp(dest2, dest1));
-    printf("\033[0;32m\"more\"\t= %d\n", ft_strcmp(dest2, dest1));
+    printf("\033[0;33mequal\t= %d\n", strcmp(dest1, dest1));
+    printf("\033[0;32mequal\t= %d\n", ft_strcmp(dest1, dest1));
+    printf("\033[0;33mless\t= %d\n", strcmp(dest1, dest2));
+    printf("\033[0;32mless\t= %d\n", ft_strcmp(dest1, dest2));
+    printf("\033[0;33mmore\t= %d\n", strcmp(dest2, dest1));
+    printf("\033[0;32mmore\t= %d\n", ft_strcmp(dest2, dest1));
     printf("\033[0m---------------------------------------------------\n");
     return (0);
 }
